e48a21370915ffe57438874d5cd6a4339224589a
Joseph Parsons <josephcpar@gmail.com>
2024-09-05 22:01:35 -0500

    Allow reading from file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Time Tracker
 
 The logo is by [Stephan](https://pixabay.com/users/io-images-1096650/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1606153) from [Pixabay](https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1606153)
+
+https://altcademy.com/blog/how-to-write-file-in-reactjs/ to write files
+https://www.w3docs.com/learn-javascript/file-and-filereader.html to read files
--- vite-project/src/App/App.tsx
+++ vite-project/src/App/App.tsx
@@ -4,6 +4,23 @@ import TimeEntryList from "../TimeEntryList/TimeEntryList";
 import TimeEntryForm from "../TimeEntryForm/TimeEntryForm";
 import { TimeEntry } from "../data/TimeEntry";
 
+function readFromFile(event: React.ChangeEvent<HTMLInputElement>) {
+  const fileList = event.target.files;
+  if (!fileList) {
+    console.log("There are no files to read from.");
+  } else {
+    const file: File = fileList[0];
+    const reader: FileReader = new FileReader();
+    reader.addEventListener("load", function (e: ProgressEvent<FileReader>) {
+      console.log(e.target?.result);
+    });
+    reader.addEventListener("error", function (e: ProgressEvent<FileReader>) {
+      console.log("Could not read file as text.");
+    });
+    reader.readAsText(file);
+  }
+}
+
 function App() {
   const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
   function addTimeEntry(timeEntry: TimeEntry) {
@@ -15,6 +32,7 @@ function App() {
       <TimeEntryForm addTimeEntry={addTimeEntry} />
       <h1>List of Entries</h1>
       <TimeEntryList timeEntries={timeEntries} />
+      <input type="file" onChange={(e) => readFromFile(e)} />
     </>
   );
 }
